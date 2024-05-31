6de923e425d9503ef83cfb8965b7e45d2807bc49
Evgen <Evgen@DESKTOP-TFNLO9V>
2024-05-31 20:23:01 +0300

    обновил маршруты для команды

--- project/routes/web.php
+++ project/routes/web.php
@@ -1,14 +1,31 @@
 <?php
+namespace App\Http\Controllers;
 
 use App\Http\Controllers\FileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\TeamsMenuController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\TeamController;
+// Маршрут для отображения формы аутентификации
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 
-Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [AuthController::class, 'login']);
+// Маршрут для аутентификации пользователя
+Route::post('/login', [LoginController::class, 'login'])->name('login');
+
+// Маршрут для выхода пользователя из приложения
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
 Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
 Route::post('/register', [RegisterController::class, 'register']);
+
+Route::get('/menu/{employee_id}', [TeamsMenuController::class, 'index'])->name('menu');
+
+Route::get('/teams', [TeamController::class, 'index'])->name('teams.index');
+Route::get('/teams/{id}', [TeamController::class, 'show'])->name('teams.show');
+Route::get('/teams/{id}/download/{filename}', [TeamController::class, 'download'])->name('teams.download');
+Route::post('/teams/{id}/upload', [TeamController::class, 'upload'])->name('teams.upload');
+Route::delete('/teams/{id}/delete/{filename}', [TeamController::class, 'deleteFile'])->name('teams.deleteFile');
 
 Route::get('/', function () {
     return view('welcome');
